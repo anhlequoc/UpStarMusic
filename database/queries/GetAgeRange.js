3c477963b237858daf605ad1694e2280bb8f396a
Anh Le <lequocanh88@gmail.com>
2018-01-27 17:58:26 +0700

    complete lecture 95

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,28 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  return Artist.find({})
+    .sort({ age: 1 })
+    .then((artists) => {
+      return {
+        min: artists[0].age,
+        max: artists[artists.length - 1].age
+      };
+    });
+
+  // solution
+  // const minQuery = Artist.find({})
+  //                   .sort({ age: 1 })
+  //                   .limit(1)
+  //                   .then((artists) => artists[0].age);
+  
+  // const maxQuery = Artist.find({})
+  //                   .sort({ age: -1 })
+  //                   .limit(1)
+  //                   .then((artists) => artists[0].age);
+
+  // return Promise.all([minQuery, maxQuery])
+  //   .then((results) => {
+  //     return { min: results[0], max: results[1] };
+  //   });
 };
--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -7,6 +7,70 @@ const Artist = require('../models/artist');
  * @param {integer} offset How many records to skip in the result set
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ * like this: {all: [artists], count: count, offset: offset, limit: limit}
  */
-module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {  
+  /* es 5 syntax
+  const sortProperty = "name";  
+  const sortOrder = {};
+  sortOrder[sortProperty] = 1;
+  console.log(sortOrder); //{name: 1}
+  */
+
+  //console.log(criteria);
+
+  //validate filters data
+  const ageMin = typeof criteria.age !== 'undefined' ? criteria.age.min : 0;
+  const ageMax = typeof criteria.age !== 'undefined' ? criteria.age.max : 100000;
+  const yearsActiveMin = typeof criteria.yearsActive !== 'undefined' ? criteria.yearsActive.min : 0;
+  const yearsActiveMax = typeof criteria.yearsActive !== 'undefined' ? criteria.yearsActive.max : 100000;
+  const searchObj = criteria.name === '' ? {} : { $text: { $search: criteria.name } };
+
+  const query = Artist.find(searchObj)
+    .where('age').gte(ageMin).lte(ageMax)
+    .where('yearsActive').gte(yearsActiveMin).lte(yearsActiveMax)
+    .sort({ [sortProperty]: 1 }) //es6 syntax: interpolated keyword
+    //note that sort is camel case, uppercase is sorted first, then lowercase
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.find(searchObj)
+    .where('age').gte(ageMin).lte(ageMax)
+    .where('yearsActive').gte(yearsActiveMin).lte(yearsActiveMax).count()])
+    .then((results) => {
+      console.log(results[0]);
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit
+      };
+    });
 };
+
+/*solution
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+};
+
+const query = Artist.find(buildQuery(criteria))
+  .sort...
+*/
